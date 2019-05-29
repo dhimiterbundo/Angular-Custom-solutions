83d13a24a65e60f32c7a7e2d7bda5f1ebf40d552
Dhimiter Bundo <b.dhimiter@yahoo.com>
2019-05-29 18:56:31 +0200

    Create menu Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponentComponent } from './layout/menu/menu-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponentComponent
   ],
   imports: [
     BrowserModule,
